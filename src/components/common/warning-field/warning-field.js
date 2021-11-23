2fb0f583f9cee4d521e13cb7aa418513bec01d5f
FrankJaskon <frankjaskon@gmail.com>
2021-11-23 11:36:54 +0200

    v0.10. Debugging and refactoring setTimeout for warnings were finished

--- src/components/common/warning-field/warning-field.js
+++ src/components/common/warning-field/warning-field.js
@@ -1,22 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import CustomButton from '../buttons/submit';
 
 import s from './WarningField.module.sass';
 
 const WarningField = ({children, hideFieldFunction, filedStyle}) => {
 
-    const [timerId, setTimerId] = useState();
-
     const onClickSkip = () => {
         hideFieldFunction('');
-        clearInterval(timerId);
     }
-
-    useEffect(() => {
-        if (children && hideFieldFunction) {
-            setTimerId(setTimeout(onClickSkip, 3000));
-        }
-    }, [children]);
 
     let requestResponse = `${s.requestResponse} ${filedStyle}`;
 
--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -1,11 +1,13 @@
 import {queryAuth} from './auth-reducer';
 
 const SET_INITIALIZATION = 'unfriendly-network/app/SET-INITIALIZATION',
-    SET_SERVER_RESPONSE = 'unfriendly-network/app/SET-SERVER-RESPONSE';
+    SET_SERVER_RESPONSE = 'unfriendly-network/app/SET-SERVER-RESPONSE',
+    SET_TIMER_ID = 'unfriendly-network/app/SET-TIMER-ID';
 
 const initialState = {
     initialized: false,
-    responseWarning: ''
+    responseWarning: '',
+    timerId: null
 };
 
 const appReducer = (state = initialState, action) => {
@@ -14,6 +16,7 @@ const appReducer = (state = initialState, action) => {
     switch (type) {
         case SET_INITIALIZATION:
         case SET_SERVER_RESPONSE:
+        case SET_TIMER_ID:
             return {
                 ...state,
                 ...action.payload
@@ -23,13 +26,32 @@ const appReducer = (state = initialState, action) => {
     }
 }
 
-export const setInitialization = (initialized) => ({type: SET_INITIALIZATION, payload: {initialized}});
-export const setResponseWarning = (responseWarning) => ({type: SET_SERVER_RESPONSE, payload: {responseWarning}});
+export const setInitialization = initialized => ({type: SET_INITIALIZATION, payload: {initialized}});
+export const setWarning = responseWarning => ({type: SET_SERVER_RESPONSE, payload: {responseWarning}});
+export const setTimerId = timerId => ({type: SET_TIMER_ID, payload: {timerId}});
 
 export const initializeApp = () => async dispatch => {
     await dispatch(queryAuth());
     await dispatch(setInitialization(true));
 
+}
+
+export const setResponseWarning = responseWarning => (dispatch, getState) => {
+
+    const id = getState().app.timerId;
+
+    if (responseWarning && !getState().app.timerId) {
+        dispatch(setWarning(responseWarning));
+        dispatch(setTimerId(setTimeout(() => {
+            dispatch(setWarning(''));
+            clearTimeout(id);
+            dispatch(setTimerId(null));
+        }, 4000)));
+    } else if (!responseWarning && getState().app.timerId) {
+        dispatch(setWarning(responseWarning));
+        clearTimeout(id);
+        dispatch(setTimerId(null));
+    }
 }
 
 export default appReducer;
