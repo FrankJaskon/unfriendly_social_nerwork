da0a8faeb8b4380c63ceddd6e2cbf0ac9cacc42e
FrankJaskon <frankjaskon@gmail.com>
2021-12-01 16:35:45 +0200

    v0.12. Added visit-me

--- src/components/aside/navbar/navbar.js
+++ src/components/aside/navbar/navbar.js
@@ -6,22 +6,27 @@ import s from './Navbar.module.sass';
 
 const Navbar = ({navbar, onClickMenuItem}) => {
     const navigation = navbar.map(page => {
-        return (
-            <li key={page.id} className={s['nav-list__item']}>
-                <NavLink to={`${page.url}`}
-                    className={s['nav-list__link']}
-                    activeClassName={s.active_link}
-                    onClick={() => onClickMenuItem()} >
-                    {page.title}
-                </NavLink>
-            </li>
-        )
-    })
-    return (
-            <ul className={s['nav-list']}>
-                {navigation}
-            </ul>
-        )
+        return <li key={page.id} className={s['nav-list__item']}>
+            <NavLink to={`${page.url}`}
+                className={s['nav-list__link']}
+                activeClassName={s.active_link}
+                onClick={() => onClickMenuItem()} >
+                {page.title}
+            </NavLink>
+        </li>
+    });
+    navigation.push(
+        <li key={'visitMe'} className={s['nav-list__item']}>
+            <NavLink to='profile/20088'
+                className={s['nav-list__link']}
+                activeClassName={s.active_link}
+                onClick={() => onClickMenuItem()} >
+                Visit me
+            </NavLink>
+        </li>)
+    return <ul className={s['nav-list']}>
+        {navigation}
+    </ul>
 }
 
 export default Navbar;
