c225e198af51a2e9b3ac538c9c029425e9d70ff7
Frank <frankjaskon@gmail.com>
2021-12-15 19:27:16 +0200

    v0.12.5. Users component was changed using hooks

--- src/components/users/old_users.js
+++ src/components/users/old_users.js
@@ -1,4 +1,4 @@
-import React,  {useEffect} from 'react';
+import React from 'react';
 import User from './user';
 import Preloader from '../common/preloader';
 import Pagination from '../common/pagination';
@@ -11,40 +11,13 @@ import {setLoadingError} from '../../redux/profile-reducer';
 import {setResponseWarning} from '../../redux/app-reducer';
 import {withRouter} from 'react-router';
 import {getResponseWarning} from '../../redux/app-selectors';
-import {getLoadingError} from '../../redux/profile-selectors';
-import {showUsers, toggleSubscription, clearUserPage} from '../../redux/users-reducer';
 
 import s from './Users.module.sass';
+import { getLoadingError } from '../../redux/profile-selectors';
 
-const Users = ({clearUserPage, showUsers, toggleSubscription, isAuth,
-    authId, usersList, usersNumber, totalPagesNumber, currentPage,
-    isFetching, usersFollowingInProgress}) => {
-
-    const showPrevPage = () => {
-        showUsers(currentPage - 1);
-    }
-    const showPrevTenPage = () => {
-        showUsers(currentPage - 10);
-    }
-    const showNextPage = () => {
-        showUsers(currentPage);
-    }
-    const showNextTenPage = () => {
-        showUsers(currentPage + 10);
-    }
-    const showNumberPage = (page) => {
-        showUsers(page);
-    }
-    const clickOnSubscription = (userId, value) => {
-        toggleSubscription(userId, value);
-    }
-
-    const isMobile = useMediaQuery({maxWidth: 767});
-
-    useEffect(() => {
-        showUsers(currentPage);
-        return clearUserPage;
-    }, []);
+const Users = ({isAuth, authId, usersList, usersNumber, totalPagesNumber, currentPage,
+    showNextPage, showNextTenPage, showPrevPage, showPrevTenPage, showNumberPage,
+    isFetching, clickOnSubscription, usersFollowingInProgress}) => {
 
     const users = usersList.map(u => <User key={u.id}
         isAuth={isAuth}
@@ -52,6 +25,8 @@ const Users = ({clearUserPage, showUsers, toggleSubscription, isAuth,
         clickOnSubscription={clickOnSubscription}
         usersFollowingInProgress={usersFollowingInProgress}
         authId={authId} />);
+
+    const isMobile = useMediaQuery({maxWidth: 767});
 
     return (
         <div className={s.users}>
@@ -92,43 +67,8 @@ const mapStateToProps = (state) => ({
     responseWarning: getResponseWarning(state)
 });
 
-const mapStateToPropsWrapper = ({auth: {id : authId, isAuth}, users: {usersList, usersNumber, totalPagesNumber,
-    currentPage, isFetching, usersFollowingInProgress}}) => {
-    return {isAuth, authId, usersList, usersNumber, totalPagesNumber,
-        currentPage, isFetching, usersFollowingInProgress}
-}
-
 export default compose(
-    connect(mapStateToPropsWrapper, {showUsers, toggleSubscription, clearUserPage}),
     connect(mapStateToProps, {setLoadingError, setResponseWarning}),
     withRouter,
     HOC.showPageErrorWrapperComponent
 )(Users);
-
-
-//     render() {
-//         const {authId, isAuth, usersList, usersNumber, totalPagesNumber, currentPage,
-//             isFetching, usersFollowingInProgress} = this.props;
-
-//         return <Users
-//             usersList={usersList}
-//             usersNumber={usersNumber}
-//             totalPagesNumber={totalPagesNumber}
-//             currentPage={currentPage}
-//             clickOnSubscription={this.clickOnSubscription}
-//             showPrevPage={this.showPrevPage}
-//             showPrevTenPage={this.showPrevTenPage}
-//             showNextPage={this.showNextPage}
-//             showNextTenPage={this.showNextTenPage}
-//             showNumberPage={this.showNumberPage}
-//             usersFollowingInProgress={usersFollowingInProgress}
-//             isAuth={isAuth}
-//             authId={authId}
-//             isFetching={isFetching} />
-//     }
-// }
-
-
-// export default compose(
-//     connect(mapStateToProps,{showUsers, toggleSubscription, clearUserPage})
-// )(UsersContainer);
